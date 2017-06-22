1a3f442b3fdc37b8d4e2cbc277ce37427aaeca14
Teresa <teresa.lenz@desy.de>
2017-06-22 18:34:44 +0200

    scripts needed for job submission and re-submission

--- histograms/resubmit.sh
+++ histograms/resubmit.sh
@@ -2,7 +2,7 @@
 
 # This script loops over all folders in directory and checks if there is a *.root file in these folders that is corrupted. If it is it is resubmitted.
 
-for dir in $(find -maxdepth 1 -type d -name "*zPt*_files")
+for dir in $(find -maxdepth 1 -type d -name "*_files")
 do
     if [[ $dir == *"xxxx"* ]]; then
 	echo ".......................... don't process this: " $dir
@@ -21,10 +21,6 @@ do
 	    echo "resubmit" $list
 	    echo reason $exit
 	    qsub $file
-            #params=$(while read line; do if [[ -n $line ]]; then if [[ ${line::1} != "#" ]]; then echo ${line}; break; fi; fi; done < $file)
-            #stringarray=($params)
-            #echo $params
-            #./qsub.sh "${stringarray[1]}" "${stringarray[2]}"
 	fi
     done
     echo ""
