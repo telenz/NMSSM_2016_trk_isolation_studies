ccb60f62bb16a36c5aebf4072f1a2baddb13a2ed
Teresa <teresa.lenz@desy.de>
2017-06-23 16:34:08 +0200

    comment out QCD sample

--- histograms/runAnalysis.sh
+++ histograms/runAnalysis.sh
@@ -13,7 +13,7 @@
 ./qsub_seq.sh analysis_macro_ztt analysisMacro_ztt_DY_ZTT.conf DYJetsToLL_M-50_13TeV-madgraphMLM_ZTT 20 &
 ./qsub_seq.sh analysis_macro_ztt analysisMacro_ztt_DY_ZLL.conf DYJetsToLL_M-10to50_13TeV-madgraphMLM_ZLL 20 &
 ./qsub_seq.sh analysis_macro_ztt analysisMacro_ztt_DY_ZLL.conf DYJetsToLL_M-50_13TeV-madgraphMLM_ZLL 20 &
-./qsub_seq.sh analysis_macro_ztt analysisMacro_ztt_MC.conf QCD_Pt-20toInf_MuEnrichedPt20_13TeV_noTrig 20 &
+#./qsub_seq.sh analysis_macro_ztt analysisMacro_ztt_MC.conf QCD_Pt-20toInf_MuEnrichedPt20_13TeV_noTrig 20 &
 ./qsub_seq.sh analysis_macro_ztt analysisMacro_ztt_DATA.conf SingleMuon_Run2016 20 &
 
 # Estimate same sign region for QCD background
