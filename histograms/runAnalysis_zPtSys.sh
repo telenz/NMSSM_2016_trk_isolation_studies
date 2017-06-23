59bfe7eb822bf5fe38b5a310504ed019b2b07e42
Teresa <teresa.lenz@desy.de>
2017-06-23 10:16:30 +0200

    cnfig parameter names changed

--- histograms/runAnalysis_zPtSys.sh
+++ histograms/runAnalysis_zPtSys.sh
@@ -1,8 +1,8 @@
 #!bin/sh
 
 uncertainty="zPtUp"
-sed -e 's/ZPtReweightingScale = 0/ZPtReweightingScale = 1/g' analysisMacro_ztt_DY_ZTT.conf > analysisMacro_ztt_DY_ZTT_${uncertainty}.conf
-sed -e 's/ZPtReweightingScale = 0/ZPtReweightingScale = 1/g' analysisMacro_ztt_DY_ZLL.conf > analysisMacro_ztt_DY_ZLL_${uncertainty}.conf
+sed -e 's/ZPtReweighting = 0/ZPtReweighting = 1/g' analysisMacro_ztt_DY_ZTT.conf > analysisMacro_ztt_DY_ZTT_${uncertainty}.conf
+sed -e 's/ZPtReweighting = 0/ZPtReweighting = 1/g' analysisMacro_ztt_DY_ZLL.conf > analysisMacro_ztt_DY_ZLL_${uncertainty}.conf
 cp DYJetsToLL_M-10to50_13TeV-madgraphMLM_ZTT DYJetsToLL_M-10to50_13TeV-madgraphMLM_ZTT_${uncertainty}
 cp DYJetsToLL_M-50_13TeV-madgraphMLM_ZTT DYJetsToLL_M-50_13TeV-madgraphMLM_ZTT_${uncertainty}
 cp DYJetsToLL_M-10to50_13TeV-madgraphMLM_ZTT DYJetsToLL_M-10to50_13TeV-madgraphMLM_ZLL_${uncertainty}
@@ -13,8 +13,8 @@ cp DYJetsToLL_M-50_13TeV-madgraphMLM_ZTT DYJetsToLL_M-50_13TeV-madgraphMLM_ZLL_$
 ./qsub_seq.sh analysis_macro_ztt analysisMacro_ztt_DY_ZLL_${uncertainty}.conf DYJetsToLL_M-50_13TeV-madgraphMLM_ZLL_${uncertainty} 20 &
 
 uncertainty="zPtDown"
-sed -e 's/ZPtReweightingScale = 0/ZPtReweightingScale = -1/g' analysisMacro_ztt_DY_ZTT.conf > analysisMacro_ztt_DY_ZTT_${uncertainty}.conf
-sed -e 's/ZPtReweightingScale = 0/ZPtReweightingScale = -1/g' analysisMacro_ztt_DY_ZLL.conf > analysisMacro_ztt_DY_ZLL_${uncertainty}.conf
+sed -e 's/ZPtReweighting = 0/ZPtReweighting = -1/g' analysisMacro_ztt_DY_ZTT.conf > analysisMacro_ztt_DY_ZTT_${uncertainty}.conf
+sed -e 's/ZPtReweighting = 0/ZPtReweighting = -1/g' analysisMacro_ztt_DY_ZLL.conf > analysisMacro_ztt_DY_ZLL_${uncertainty}.conf
 cp DYJetsToLL_M-10to50_13TeV-madgraphMLM_ZTT DYJetsToLL_M-10to50_13TeV-madgraphMLM_ZTT_${uncertainty}
 cp DYJetsToLL_M-50_13TeV-madgraphMLM_ZTT DYJetsToLL_M-50_13TeV-madgraphMLM_ZTT_${uncertainty}
 cp DYJetsToLL_M-10to50_13TeV-madgraphMLM_ZTT DYJetsToLL_M-10to50_13TeV-madgraphMLM_ZLL_${uncertainty}
