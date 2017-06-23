87f51262e4a36bcb1c56054439bba61bceea7f0c
Teresa <teresa.lenz@desy.de>
2017-06-23 13:34:38 +0200

    add fit

--- datacards/plotAndFitScaleFactors.C
+++ datacards/plotAndFitScaleFactors.C
@@ -1,28 +1,42 @@
 #include <iostream>
-#include "HttStylesNew.cc"
+#include "../plotting/HttStylesNew.cc"
 #include "TGraphErrors.h"
-
+#include "TF1.h"
 
 void plotAndFitScaleFactors(){
 
+  SetStyle();
+
   const int ptBins = 5;
-  float x[ptBins +1]    = {3.75 , 6.25 , 8.75 , 15 , 30};
-  float xErr[ptBins +1] = {1.25 , 1.25 , 1.25 , 5 , 10};
-  float y[ptBins +1]    = {7.5879 , 1.9226 , 1.0177 , 0.97227 , 0.77155 };
-  float yErr[ptBins +1] = {0.198  , 0.201  , 0.0822 , 0.0366  , 0.023};
+  float x[ptBins +1]    = {6.25  , 8.75  , 12.5  , 17.5  , 30   };
+  float xErr[ptBins +1] = {1.25  , 1.25  , 2.5   , 2.5   , 10   };
+  float y[ptBins +1]    = {0.650 , 0.643 , 0.785 , 0.698 , 0.804};
+  float yErr[ptBins +1] = {0.069 , 0.038 , 0.035 , 0.034 , 0.042};
 
   TGraphErrors *g =  new TGraphErrors(ptBins,x,y,xErr,yErr);
   g->SetMarkerStyle(20);
   g->GetXaxis()->SetTitle("track pt");
   g->GetYaxis()->SetTitle("trk iso scale factor");
   g->SetTitle("");
+  g->GetYaxis()->SetRangeUser(0.5,1.0);
 
+  // Make Fit
+  TF1* f1 = new TF1("f1","[0]");
+  f1->SetLineColor(kOrange);
+  g->Fit("f1");
+  /*
+  TF1* f2 = new TF1("f2","[0]*x+[1]");
+  f2->SetLineColor(kBlue);
+  g->Fit("f2");
+  */
 
   TCanvas * canv1 = MakeCanvas("canv1", "", 700, 800);
   canv1 -> cd();
 
   g->Draw("AP");
 
-  canv1->SaveAs("TauIsolation_ScaleFactos.pdf");
+  
+
+  canv1->SaveAs("TauIsolation_ScaleFactors.pdf");
 
 }
