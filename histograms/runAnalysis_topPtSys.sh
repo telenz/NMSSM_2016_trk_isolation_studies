c014c21d9c701fd155e6a71b4e8002e887fe2c56
Teresa <teresa.lenz@desy.de>
2017-06-23 10:16:27 +0200

    cnfig parameter names changed

--- histograms/runAnalysis_topPtSys.sh
+++ histograms/runAnalysis_topPtSys.sh
@@ -1,12 +1,12 @@
 #!bin/sh
 
 uncertainty="topPtUp"
-sed -e 's/TopPtReweightingScale = 0/TopPtReweightingScale = 1/g' analysisMacro_ztt_TT.conf > analysisMacro_ztt_TT_${uncertainty}.conf
+sed -e 's/TopPtReweighting = 0/TopPtReweighting = 1/g' analysisMacro_ztt_TT.conf > analysisMacro_ztt_TT_${uncertainty}.conf
 cp TT_13TeV-powheg-pythia8 TT_13TeV-powheg-pythia8_${uncertainty}
 ./qsub_seq.sh analysis_macro_ztt analysisMacro_ztt_TT_${uncertainty}.conf TT_13TeV-powheg-pythia8_${uncertainty} 20 &
 
 uncertainty="topPtDown"
-sed -e 's/TopPtReweightingScale = 0/TopPtReweightingScale = -1/g' analysisMacro_ztt_TT.conf > analysisMacro_ztt_TT_${uncertainty}.conf
+sed -e 's/TopPtReweighting = 0/TopPtReweighting = -1/g' analysisMacro_ztt_TT.conf > analysisMacro_ztt_TT_${uncertainty}.conf
 cp TT_13TeV-powheg-pythia8 TT_13TeV-powheg-pythia8_${uncertainty}
 ./qsub_seq.sh analysis_macro_ztt analysisMacro_ztt_TT_${uncertainty}.conf TT_13TeV-powheg-pythia8_${uncertainty} 20 &
 
